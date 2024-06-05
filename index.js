56cfa1fe7545a004b26def0b63a856ea01637016
TERRAS JUBA p2212511 <juba.terras@etu.univ-lyon1.fr>
2024-06-05 07:26:32 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let http = require('http');
+
+let server = http.createServer((req, res) => {
+    // Get the IP address of the sender
+    let ip = req.socket.remoteAddress;
+
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send the IP address as the response
+    res.end(`Your IP address is: ${ip}`);
+});
+
+// Define the port to listen on
+let PORT = 80;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
