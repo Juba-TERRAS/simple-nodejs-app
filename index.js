40dc7e8694bce5b750f269724e18d2fe78e3b866
TERRAS JUBA p2212511 <jubaterras600@gmail.com>
2024-06-05 08:38:23 +0100

    Fifth commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let http = require('http');
 
 let server = http.createServer((req, res) => {
+    // Get the IP address of the sender
+    let ip = req.socket.remoteAddress;
+
     // Set the response header
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     // Send the IP address as the response
-    res.end(`hello world`);
+    res.end(`Your IP address is: ${ip}`);
 });
 
 // Define the port to listen on
-let PORT = 80;
+let PORT = 8080;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
