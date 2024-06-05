80cd9e58dc641fa0ab71988d9ea38dd386866405
TERRAS JUBA p2212511 <jubaterras600@gmail.com>
2024-06-05 08:26:42 +0100

    Forth commit

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ let server = http.createServer((req, res) => {
 let PORT = 80;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
